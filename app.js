df2e9da2553290c2f1e12b388951dcd1a11b41c0
JamiePMortimer <16468666+JamiePMortimer@users.noreply.github.com>
2021-01-17 15:54:45 +0000

    Finished Project

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const navClick = document.querySelector('.worlds');
+const backdrop = document.querySelector('.backdrop');
 
 navClick.addEventListener('click', () =>{
   document.querySelector('.langModal').classList.toggle('hide');
+  document.querySelector('.backdrop').classList.toggle('hide');
+})
+backdrop.addEventListener('click', () =>{
+  document.querySelector('.langModal').classList.add('hide');
+  document.querySelector('.backdrop').classList.add('hide');
 })
